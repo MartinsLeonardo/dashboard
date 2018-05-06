8a4b90c0533230466da71d3b272ed078eab16157
Leonardo Martins <leodms789@gmail.com>
2018-05-06 02:26:05 -0300

    Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { OperacionalComponent } from './operacional/operacional.component';
+import { AnaliticoComponent } from './analitico/analitico.component';
+import { EstrategicoComponent } from './estrategico/estrategico.component';
 
+const appRoutes: Routes = [
+  {path: 'operacional', component: OperacionalComponent},
+  {path: 'analitico', component: AnaliticoComponent},
+  {path: 'estrategico', component: EstrategicoComponent},
+  {path: '', component: MenuComponent},
+  {path: '**', component: MenuComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    OperacionalComponent,
+    AnaliticoComponent,
+    EstrategicoComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} 
+    ),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/estrategico/estrategico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estrategico',
+  templateUrl: './estrategico.component.html',
+  styleUrls: ['./estrategico.component.css']
+})
+export class EstrategicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
